14440827401a2529976315b920ae8cdb4a4618ab
Kaan EFE <kaefean1@gmail.com>
2023-08-27 06:28:31 +0300

    test folder fix

--- test/degisken_test.dart
+++ test/degisken_test.dart
@@ -6,16 +6,14 @@ import 'package:tmdb_movie/core/enum/request_type_enum.dart';
 import 'package:tmdb_movie/core/enum/tmdb_query_parameter_enum.dart';
 import 'package:tmdb_movie/core/extension/tmdb_query_parameter_extension.dart';
 import 'package:tmdb_movie/core/model/tmdb_models/movie_model/movie_model.dart';
-import 'package:tmdb_movie/core/model/tmdb_models/movie_tv_utilts_model/movie_result_model/movie_result_model.dart';
 import 'package:tmdb_movie/core/model/tmdb_models/movie_tv_utilts_model/movie_tv_search_result_generic_model/movie_tv_search_result_generic_model.dart';
 import 'package:tmdb_movie/core/model/tmdb_models/movie_tv_utilts_model/tv_utilts/tv_show_result_model/tv_show_result_model.dart';
 import 'package:tmdb_movie/core/model/tmdb_models/tv_show_model/tv_show_model.dart';
-import 'package:tmdb_movie/core/model/tmdb_models/tv_show_season_model/tv_show_season_model.dart';
 import 'package:tmdb_movie/core/network/network_manager.dart';
 
 void main() {
-  final asd = <String, dynamic>{};
-  asd.addEntries([
+  final queryParam = <String, dynamic>{};
+  queryParam.addEntries([
     TmdbQueryParameter.ATR.parameter(),
   ]);
   final options = BaseOptions(
@@ -25,7 +23,7 @@ void main() {
       'accept': 'application/json',
       'content-type': 'application/json',
     },
-    queryParameters: asd,
+    queryParameters: queryParam,
   );
   test('movie request', () async {
     final dio = Dio(options);
@@ -55,17 +53,17 @@ void main() {
 
     debugPrint(result.results![0].name);
   });
-  test('season request test', () async {
-    final asd = BaseOptions(baseUrl: 'https://jsonplaceholder.typicode.com');
-    final seasonRequest = Dio(asd);
-    final response = await seasonRequest.request(
-      '/posts',
-      data: const SeasonModel().toJson(),
-    );
-    // final result = const SeasonModel().fromJson(response.data );
-    debugPrint(response.data.runtimeType.toString());
-    // debugPrint(result.episodes![0].overview);
-  });
+  // test('season request test', () async {
+  //   final asd = BaseOptions(baseUrl: 'https://jsonplaceholder.typicode.com');
+  //   final seasonRequest = Dio(asd);
+  //   final response = await seasonRequest.request<dynamic>(
+  //     '/posts',
+  //     data: const SeasonModel().toJson(),
+  //   );
+  //    final result = const SeasonModel().fromJson(response.data );
+  //   debugPrint(response.data.runtimeType.toString());
+  //    debugPrint(result.episodes![0].overview);
+  // });
   test('basic network layer test', () async {
     final service = NetworkManager<TvShowModel>(options: options);
     final result = await service.send<MovieModel, MovieModel>(
